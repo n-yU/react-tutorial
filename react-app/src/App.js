77e1c2345f17c0f8a0680f9ab00d00c32ee6973b
nyU <51310314+n-yU@users.noreply.github.com>
2023-06-13 17:38:01 +0900

    Add ShoppingList

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -23,44 +23,68 @@ import './App.css';
 // }
 
 function MyButton() {
-  return (
-    <button>
-      I'm a button
-    </button>
-  );
+    return (
+        <button>
+            I'm a button
+        </button>
+    );
 }
 
 function MyApp() {
-  return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
-  );
+    return (
+        <div>
+            <h1>Welcome to my app</h1>
+            <MyButton />
+        </div>
+    );
 }
 
 const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
 };
 
 function Profile() {
-  return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }}
-      />
-    </>
-  );
+    return (
+        <>
+            <h1>{user.name}</h1>
+            <img
+                className="avatar"
+                src={user.imageUrl}
+                alt={'Photo of ' + user.name}
+                style={{
+                    width: user.imageSize,
+                    height: user.imageSize
+                }}
+            />
+        </>
+    );
+}
+
+const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
+];
+
+function ShoppingList() {
+    const listItems = products.map(product =>
+        <li
+            key={product.id}
+            style={{
+                color: product.isFruit ? 'magenta' : 'darkgreen'
+            }}
+        >
+            {product.title}
+        </li>
+    );
+
+    return (
+        <ul>{listItems}</ul>
+    );
 }
 
 // export default MyApp;
-export default Profile;
+// export default Profile;
+export default ShoppingList;
